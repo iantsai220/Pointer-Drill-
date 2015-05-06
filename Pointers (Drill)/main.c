fd226b395e08ed9a4d185e7f6db502cb8227a121
iantsai <iantsai86@gmail.com>
2015-05-06 16:32:57 -0700

    Initial Commit

---
+++ Pointers (Drill)/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Pointers (Drill)
+//
+//  Created by Ian Tsai on 2015-05-06.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
